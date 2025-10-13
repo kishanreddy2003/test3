9b0629d405a33b5615d4785fb228c3283ee48931
kishan <kishanreddy226@gmail.com>
2025-10-13 15:17:32 +0530

    detect-fix

--- src/main/java/com/test3/Test3Application.java
+++ src/main/java/com/test3/Test3Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test3Application {
 
 	public static void main(String[] args) {
+		String name="mike";
+		int x=100;
+		bollean isPresent=false;
+		int y=200;
+		char x='a';
+		int y=200;
+		double z=10.3;
+		boolean isPresent=false;
+		float m=10.3f;
 		SpringApplication.run(Test3Application.class, args);
 	}
 
